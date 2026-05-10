0ffa8b6a4ef2a95f379e6d493c98614afd93f5db
aouladhajyassmina-arch <aouladhajyassmina@gmail.com>
2026-05-11 00:24:30 +0100

    dft

--- api/check.php
+++ api/check.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
     } else {
         $_SESSION['error'] = "Identifiant ou Mot de passe incorrect!";
-        header("Location: index.php");
+        header("index.php");
         
     }
 }
