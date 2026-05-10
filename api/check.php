926a08db2511199c11e918b8e63491f14c59793a
aouladhajyassmina-arch <aouladhajyassmina@gmail.com>
2026-05-11 00:12:30 +0100

    fleur

--- api/check.php
+++ api/check.php
@@ -1,24 +1,25 @@
-
- <?php 
+<?php
+// لا تترك أي سطر فارغ هنا فوق!
 session_start();
-    
-$id_ = 'Yasmina';
-$password_ = '2007';
+
+$id_valide = 'Yasmina';
+$pass_valide = '2007';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $id = $_POST['id'];
-    $password = $_POST['password'];
+    $id = isset($_POST['id']) ? trim($_POST['id']) : '';
+    $password = isset($_POST['password']) ? trim($_POST['password']) : '';
     
-    if ($id_ === $id && $password_ === $password) {
+    if ($id === $id_valide && $password === $pass_valide) {
         $_SESSION['user'] = $id;          
         $_SESSION['logged_in'] = true;     
         
+        // التحويل يجب أن يكون قبل أي echo أو HTML
         header("Location: store.php");
         exit();
-    }
-    else {
-        echo 'ID ou MOT DE PASSE incorrecte!! <br>';
-        echo '<a href="index.php">Réssayer</a>';
+    } else {
+        $_SESSION['error'] = "Identifiant ou Mot de passe incorrect!";
+        header("Location: index.php");
+        exit();
     }
 }
 ?>
