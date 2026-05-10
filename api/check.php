d3163c08943c0102bbc8e122a3fbcc9d5adf45d0
aouladhajyassmina-arch <aouladhajyassmina@gmail.com>
2026-05-11 00:15:15 +0100

    flriu

--- api/check.php
+++ api/check.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         $_SESSION['error'] = "Identifiant ou Mot de passe incorrect!";
         header("Location: index.php");
-        exit();
+        
     }
 }
 ?>
